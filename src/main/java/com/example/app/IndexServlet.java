1f7f69579dc2579db9ad87c9c5ca0d61036e6539
yodal <yodal.y8n@gmail.com>
2025-10-17 10:04:15 +0900

    초기 테스트 완료

---
+++ src/main/java/com/example/app/IndexServlet.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet({"/","/index"})
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: USER
+  Date: 2025-10-17
+  Time: 오전 10:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>가제</title>
+</head>
+<body>
+    <h1>프로젝트</h1>
+</body>
+</html>
